74e555ba204467d811d302bb19ab46b489d5b5fc
vikash sharma <vikashtiwari9893@gmail.com>
2026-03-31 23:49:08 +0530

    code

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 👈 This removes the debug banner
-      title: "Counter App",
+      title: "Counter App Flutter",
       home: CounterPage(),
     );
   }
